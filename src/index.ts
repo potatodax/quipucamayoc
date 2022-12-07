d1c362ac267e241a5aa3057d3055d75630c8b6eb
potatodax <86934143+potatodax@users.noreply.github.com>
2022-12-07 16:47:13 -0600

    remove tester SVG of donut

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-// import svgDonut from "bundle-text:../assets/donut.svg";
 import { renderSceneGraph } from "./render";
 import { createSceneGraph } from "./sceneGraph";
 import { PendantCord } from "./utils";
-
-// element.append(svgDonut);
 
 // data comes from self.data in quipucamayoc.py
 const pendantCordsData: PendantCord[] = data.map((cord) => ({
